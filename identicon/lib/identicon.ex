70d35b0a308452bacd492c66aa7bc7e2ba3e5aac
Leonardo Cardoso <leocardosoti@gmail.com>
2018-08-16 19:49:40 -0300

    getting the rgb

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,21 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
 
-  @doc """
-  Hello world.
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
 
-  ## Examples
+    [r, g, b]
+  end
 
-      iex> Identicon.hello
-      :world
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
-  """
-  def hello do
-    :world
+    %Identicon.Image{hex: hex}
   end
 end
